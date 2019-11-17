642e17ef60aac40d60e1cf5808827153e2a1be80
yuhui1998 <1176951680@qq.com>
2019-11-17 22:23:30 +0800

    下一次增加代理池

--- meituan_dynamic_render.py
+++ meituan_dynamic_render.py
@@ -6,5 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 browser = webdriver.Chrome()
 browser.get('https://www.meituan.com/meishi/41007600/')
-print(browser.page_source)
+#商铺名字
+shopName = browser.find_element_by_class_name('details').find_element_by_class_name('name')
+shopNameText = shopName.text.replace('食品安全档案','')
+shopNameText= shopNameText.replace('/n','')
+#网友点评的标签
+commentTags = browser.find_element_by_css_selector('.tags.clear li')
+#获取评论页码
+commentsPage = browser.find_element_by_css_selector('.pagination.clear li:nth-last-child(2)')
+print(shopNameText,commentTags.text,commentsPage.text)
 browser.close()
